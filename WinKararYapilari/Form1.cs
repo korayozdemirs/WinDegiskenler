9e7cce3d8c6858ebd1358fd4aaae0275b9d43038
korayozdemir <koray_ozdemir@msn.com>
2020-05-14 13:31:38 +0300

    Karar Yapıları Bolum 1

---
+++ WinKararYapilari/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinKararYapilari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = int.Parse(textBox1.Text);
+            //eğer sayi buyuktur 10 ise mesaj : sayı 10 dan buyuktur
+            //değilse mesaj : Sayı 10 dan küçüktür
+            //else kontrol edecek başka ihtimal olmam durumunda kullanılır.
+            //yada gerşiye kalan üm durumlar için.
+            if (sayi > 10)
+            {
+                MessageBox.Show("Sayı 10 dan büyüktür");
+            }
+            else if (sayi == 10)
+            {
+                MessageBox.Show("Sayı 10 a eşittir");
+            }
+            else 
+            {
+                MessageBox.Show("Sayı 10 dan küçüktür");
+            }
+
+        }
+    }
+}
---
+++ WinKararYapilari/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinKararYapilari
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = int.Parse(textBox1.Text);
+            //sayının 2 ile bölümünden kalan 0 ise
+            if (sayi < 0)
+            {
+                MessageBox.Show("Pozitif Sayı Giriniz");
+            }
+            else if (sayi % 2 == 0)
+            {
+                MessageBox.Show("Sayı Çifttir");
+            }
+            else
+            {
+                MessageBox.Show("Sayı tektir");
+            }
+        }
+    }
+}
