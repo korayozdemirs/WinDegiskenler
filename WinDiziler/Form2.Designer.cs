ef48cac31b65857e1feb9dac948c85ff9bedbbe9
korayozdemir <koray_ozdemir@msn.com>
2020-05-27 14:06:41 +0300

    Foreach

--- WinDiziler/Form2.Designer.cs
+++ WinDiziler/Form2.Designer.cs
@@ -28,20 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "En buyuk Sayı";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(219, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(280, 372);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Listele";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 48);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "En Kucuk Sayı";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(26, 201);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(170, 57);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Ortalama Değer";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(27, 264);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(170, 57);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Ortalamadan Buyukler";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(28, 327);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(170, 57);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Listele";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(535, 80);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(203, 45);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "Ekle";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(535, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(535, 131);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(203, 48);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "Listele";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(535, 185);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(204, 53);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "Listele";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
--- WinDiziler/Form2.cs
+++ WinDiziler/Form2.cs
@@ -16,13 +16,146 @@ namespace WinDiziler
         {
             InitializeComponent();
         }
-
+        int[] sayilar = { 33, 12, 55, 65, 13, 46, 78, 32, 1, 4, 72 };
         private void Form2_Load(object sender, EventArgs e)
         {
             //form açılışında bir dizi tanımlayıp içine 
             //33,12,55,65,13,46,78,32,1,4,72 değerlerini atayın.
-            int[] sayilar = { 33, 12, 55, 65, 13, 46, 78, 32, 1, 4, 72 };
-            sayilar[3] = 9999;
+            //sayilar[3] = 9999;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int max = sayilar[0];
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    if (sayilar[i] > max)
+            //    {
+            //        max = sayilar[i];
+            //    }
+            //}
+            foreach (int sayi in sayilar)
+            {
+                if (sayi > max)
+                {
+                    max = sayi;
+                }
+            }
+            MessageBox.Show(max.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //listele
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    listBox1.Items.Add(sayilar[i]);
+            //}
+
+            foreach (int sayi in sayilar)
+            {
+                listBox1.Items.Add(sayi);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int min = sayilar[0];
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    if (sayilar[i] < min)
+            //    {
+            //        min = sayilar[i];
+            //    }
+            //}
+
+            foreach (int sayi in sayilar)
+            {
+                if (sayi < min)
+                {
+                    min = sayi;
+                }
+            }
+            MessageBox.Show(min.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            foreach (int sayi in sayilar)
+            {
+                toplam += sayi;
+                //toplam = toplam + sayi;
+            }
+            decimal ortalama = (toplam / sayilar.Length);
+            MessageBox.Show(ortalama.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            int toplam = 0;
+            foreach (int sayi in sayilar)
+            {
+                toplam += sayi;
+                //toplam = toplam + sayi;
+            }
+            decimal ortalama = (toplam / sayilar.Length);
+
+            foreach (int sayi in sayilar)
+            {
+                if (sayi > ortalama)
+                {
+                    listBox1.Items.Add(sayi);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //"Ahmet","Mehmet","Dursun","Fatma","Cevriye"
+            string[] isimler = { "Ahmet", "Mehmet", "Dursun", "Fatma", "Cevriye" };
+            foreach (string isim in isimler)
+            {
+                listBox1.Items.Add(isim);
+            }
+        }
+        int[] liste = new int[0];
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int sayi = int.Parse(textBox1.Text);
+            Array.Resize(ref liste, liste.Length + 1);
+            liste[liste.Length - 1] = sayi;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            foreach (var sayi in liste)
+            {
+                listBox1.Items.Add(sayi);
+            }
+        }
+
+        int[] cikanSayiler = new int[6];
+        int sayac = 0;
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            while (sayac < 6)
+            {
+                int uretilen = rnd.Next(1, 11);
+                //uretilen deger cikanlar listesinde yoksa
+                if (Array.IndexOf(cikanSayiler, uretilen) == -1)
+                {
+                    cikanSayiler[sayac] = uretilen;
+                    sayac++;
+                }
+            }
+
+            for (int i = 0; i < cikanSayiler.Length; i++)
+            {
+                listBox1.Items.Add(cikanSayiler[i]);
+            }
         }
     }
 }
