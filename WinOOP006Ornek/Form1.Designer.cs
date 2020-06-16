7a3796e66694a01ecd668cda7a9fa80d6088f8a4
korayozdemir <koray_ozdemir@msn.com>
2020-06-16 14:04:10 +0300

    Odev

---
+++ WinOOP006Ornek/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WinOOP006Ornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürün Adı";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(109, 20);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(182, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Fiyat";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(109, 59);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(182, 22);
+            this.txtPrice.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kategori";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(108, 98);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(183, 24);
+            this.cmbCategory.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(27, 162);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(879, 240);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Adı";
+            this.columnHeader1.Width = 189;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Fiyat";
+            this.columnHeader2.Width = 178;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Kategori";
+            this.columnHeader3.Width = 315;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 414);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ WinOOP006Ornek/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinOOP006Ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Category> categoryList;
+        List<Product> productList = new List<Product>();
+        int counterId = 1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            categoryList = new List<Category>()
+            {
+                new Category{Id = 1,Name="Elektronik"},
+                new Category{Id = 2,Name="Giyim"},
+                new Category{Id = 3,Name="Temizlik"},
+                new Category{Id = 4,Name="İçecek"}
+            };
+
+            cmbCategory.DataSource = categoryList;
+            cmbCategory.DisplayMember = "Name";
+            cmbCategory.ValueMember = "Id";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            p.Id = counterId;
+            p.Name = txtName.Text;
+            p.Price = decimal.Parse(txtPrice.Text);
+            //p.CategoryId = ((Category)cmbCategory.SelectedItem).Id;
+            p.Category = (Category)cmbCategory.SelectedItem;
+            counterId++;
+            productList.Add(p);
+            DataFill();
+        }
+
+        private void DataFill()
+        {
+            foreach (Product product in productList)
+            {
+                ListViewItem li = new ListViewItem(product.Name);
+                li.SubItems.Add(product.Price.ToString());
+                li.SubItems.Add(product.Category.Name);
+                listView1.Items.Add(li);
+            }
+        }
+    }
+}
